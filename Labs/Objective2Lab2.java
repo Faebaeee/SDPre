f0de42e1a3385039cdf808333496eb545593fa76
Faith Cicero <faithmates98@gmail.com>
2022-01-20 11:25:20 -0700

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Faith Cicero";
+    int age = 23;
+    String state = "Connecticut";
+
+    System.out.println("Name: " + name + " " + "Age: " + age + " " + "State: " + state);
+  }
+}
