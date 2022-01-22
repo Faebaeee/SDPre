a2e419f8026ff247a5a792059b757f4eef3a70f8
Faith Cicero <faithmates98@gmail.com>
2022-01-22 14:05:56 -0700

    objective3lab1.java

--- Labs/Objective3Lab1.java
+++ Labs/Objective3Lab1.java
@@ -2,7 +2,7 @@ public class Objective3Lab1 {
   public static void main(String[] args) {
     double num1 =34, num2 =20, num3 =65, average;
 
-    average = (num1 + num2 + num3) / 3;
+    average = (num1 + num2 + num3) /3;
 
     System.out.println("The average of the three numbers: " + average);
   }
